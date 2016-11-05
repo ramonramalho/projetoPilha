359315dc60b6b59bb5787e5709e6174f5bf0533d
Sérgio Ferreira <sergio-fh@hotmail.com>
2016-11-04 22:18:59 -0200

    Add files via upload

--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@ void inverterString(char **string){
 	pilha *p;
 	Inicializar_pilha(&p);
 	int i=0, j=0;
-	while((*string)[i] != '\0'){
+	while( (*string)[i] != '\0'){
 		Inserir_topo(&p, (int)(*string)[i]);
 		i++;
 	}
@@ -23,12 +23,12 @@ void inverterString(char **string){
 int main(){
 	printCabecalho();
 	pilha *p;
-	int escolha=0;
+	char escolha[100];
 	do{
 		printMenu();
-		scanf("%d", &escolha);
-		switch(escolha){
-			case 1:
+		fgets(escolha, 100, stdin);
+		switch(escolha[0]){
+			case '1':
 				{
 					int info=0;
 					printf("Digite o valor a ser acrescentado: \n");
@@ -36,40 +36,42 @@ int main(){
 					Inserir_topo(&p, info);
 				}
 				break;
-			case 2:
+			case '2':
 				Remover_topo(&p);
 				break;
-			case 3:
+			case '3':
 				inverter(&p);
 				break;
-			case 4:
+			case '4':
 				{
 					int tamanho = tamanhoPilha(p);
 					printf("Tamanho: %d\n", tamanho);
 				}
 				break;
-			case 5:
+			case '5':
 				{
 					int ver;
 					Verifica_vazio(p, &ver);
 					if(ver==0){
 						printf("Pilha não está vazia!\n");
 					}else{
-						printf("Pilha está vazia\n");	
+						printf("Pilha está vazia\n");
 					}
 				}
 				break;
-			case 6:
+			case '6':
 				{
-					char string[300] = {0};
+					char* string = (char*) malloc(sizeof(char) * 300);
 					fgets(string, 300, stdin);
+					
 					int i=0; for(; i<300; i++) if(string[i]=='\n'){ string[i]='\0'; break; } //tirar \n
 					inverterString((char**)&string);
 					printf("%s\n", string);
+					free(string);
 				} 
 				break;
 		}
-	} while(escolha != 7);
+	} while(escolha[0] != '7');
 	
 	Inicializar2_pilha(&p);
 	return 0;
